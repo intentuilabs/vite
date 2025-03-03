b198717df75e2f9ef76df42444347fce6c914de8
Irsyad A. Panjaitan <irsyad@parsinta.com>
2025-03-03 15:28:18 +0700

    Clear cached files

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,1 +1,7 @@
+export * from "./primitive";
+export * from "./button";
 export * from "./container";
+export * from "./dialog";
+export * from "./loader";
+export * from "./primitive";
+export * from "./toast";
